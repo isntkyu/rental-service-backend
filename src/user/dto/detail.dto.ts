7383a9101007e7547bc26eece26f94750de8f86c
이준규 <jkjkjk5710@gmail.com>
2023-09-21 02:27:49 +0900

    관계 변경에 따른 api fix

--- src/user/dto/detail.dto.ts
+++ src/user/dto/detail.dto.ts
@@ -4,6 +4,7 @@ import { RENTAL_STATUS } from '../../rental/entities/rental.entity';
 import { UserWithRentalList } from '../user.service';
 import { Type } from 'class-transformer';
 import { format } from 'date-fns';
+import { sortBy } from 'lodash';
 import { RentalListItem } from '../../rental/dto/list.dto';
 
 export class UserDetailParam {
@@ -35,20 +36,23 @@ export class GeneralUserDetailResDto {
     this.userId = user.userId;
     this.email = user.email;
     this.name = user.name;
+    const sortedRentals = sortBy(user.rentals, ['rentalId']).reverse();
+
     this.rentalInfo =
-      user.rentals[0] == null || user.rentals[0]?.status === RENTAL_STATUS.PAID
+      sortedRentals[0] == null ||
+      sortedRentals[0]?.status === RENTAL_STATUS.PAID
         ? null
         : {
-            status: user.rentals[0].status,
-            rentalId: user.rentals[0].rentalId,
-            serialNumber: user.rentals[0].serialNumber,
-            businessCode: user.rentals[0].businessCode,
-            rentalDate: format(user.rentals[0].rentalDate, 'yyyy.MM.dd'),
+            status: sortedRentals[0].status,
+            rentalId: sortedRentals[0].rentalId,
+            serialNumber: sortedRentals[0].serialNumber,
+            businessCode: sortedRentals[0].businessCode,
+            rentalDate: format(sortedRentals[0].rentalDate, 'yyyy.MM.dd'),
             returnDate:
-              user.rentals[0].returnDate == null
+              sortedRentals[0].returnDate == null
                 ? ''
-                : format(user.rentals[0].returnDate, 'yyyy.MM.dd'),
-            price: user.rentals[0].price,
+                : format(sortedRentals[0].returnDate, 'yyyy.MM.dd'),
+            price: sortedRentals[0].price,
           };
   }
 }
