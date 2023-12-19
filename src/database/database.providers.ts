014b3991bcd6f538c1c9410407e28e6a5959ce44
이준규 <jkjkjk5710@gmail.com>
2023-12-20 00:04:16 +0900

    dot env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,6 +7,13 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
+        host: process.env.host,
+        port: +process.env.port,
+        username: process.env.username,
+        password: process.env.password,
+        database: process.env.database,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        logging: true,
       });
 
       return dataSource.initialize();
